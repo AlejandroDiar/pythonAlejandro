90b73131cdb9ab494eb1fe35097440cb6a8fd023
Alejandro <adiaari1810@ieszaidinvergeles.org>
2025-10-09 09:43:32 +0200

    Ejercicio8

---
+++ Ejercicio8.py
@@ -0,0 +1,5 @@
+n=int(input("Introduce el primero numero entero: "))
+m=int(input("Introduce el segundo numero entero: "))
+c= n / m
+r = n % m
+print ("El cociente da " + str(c) + " y el resto " + str(r) + ".")
