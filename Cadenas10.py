f9af537dcce7b48f37da4956220ceb23751015f8
Alejandro <adiaari1810@ieszaidinvergeles.org>
2025-10-23 10:05:08 +0200

    Cadena 10

---
+++ Cadenas10.py
@@ -0,0 +1,9 @@
+#Escribir un programa que pregunte por consola por los productos de una cesta de la
+#compra, separados por comas, y muestre por pantalla cada uno de los productos en
+#una línea distinta.
+
+productos = input ("Introduce los productos a comprar separados con comas: ")
+p = productos.split(",")
+print ("\n".join(p))
+
+
