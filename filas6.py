8753d40bf5bddc57da4d3ba736041ffcff874d49
Alejandro <adiaari1810@ieszaidinvergeles.org>
2026-01-08 10:19:13 +0100

    filas6

---
+++ filas6.py
@@ -0,0 +1,20 @@
+asignaturas = ["Matemáticas","Física","Química","Historia","Lengua"]
+aprobadas = []
+notas = []
+
+for i in asignaturas:
+    nota = int(input("Qué nota sacaste en "+i+"?"))
+    notas.append(nota)
+    if nota >= 5:
+        aprobadas.append(i)
+
+for i in aprobadas:
+    asignaturas.remove(i)
+longitud = len(asignaturas)
+if longitud < 1:
+    print ("Has aprobado todas.")
+else:
+    print ("Asignaturas a repetir:")
+    for i in asignaturas:
+        print (i)
+
